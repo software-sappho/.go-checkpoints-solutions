ee9c240af808cf6bd646bfe42dbd8b3b91195760
astas <adriana.stas00@gmail.com>
2024-11-15 13:29:33 +0200

    modified onlya.go

--- onlya/onlya.go
+++ onlya/onlya.go
@@ -6,4 +6,6 @@ func main() {
 	z01.PrintRune('a')
 	// z01.PrintRune('z') for onlyz.go
 	// z01.PrintRune('1') for only1.go
+	// z01.PrintRune('b') for onlyb.go
+	// z01.PrintRune('f') for onlyf.go
 }
