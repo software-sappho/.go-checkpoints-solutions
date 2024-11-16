9389df85a7c1ab99a6b6cb98cd06bca082e35252
astas <adriana.stas00@gmail.com>
2024-11-16 13:00:31 +0200

    firstword.go

---
+++ firstword/firstword.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func FirstWord(s string) string {
+	//sliced := []rune
+	first := []rune{}
+
+	for _, i := range s {
+		if i != ' ' {
+			first = append(first, i)
+		} else {
+			break
+		}
+	}
+
+	return string(first)
+}
+
+func main() {
+	fmt.Println(FirstWord("hello there"))
+	fmt.Println(FirstWord(""))
+	fmt.Println(FirstWord("hello   .........  bye"))
+}
