da59005255908aeeee4f655acfe4722ad6e987ac
astas <andriana.stas00@gmail.com>
2024-11-21 15:41:37 +0200

    updated digitlen.go

--- 20%/digitlen/digitlen.go
+++ 20%/digitlen/digitlen.go
@@ -7,19 +7,19 @@ import (
 func DigitLen(n, base int) int {
 	var count int
 
-	if base < 2 || base > 36 {
+	if base < 2 || base > 36{
 		return -1
 	}
 
-	if n < 0 {
+	if n < 0{
 		n = -n
 	}
 
-	count = 0
-	for n > 0 {
-		n /= base
+	for n>0{
+		n/=base
 		count++
 	}
+
 	return count
 }
 
