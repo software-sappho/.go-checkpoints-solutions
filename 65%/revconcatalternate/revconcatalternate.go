edcfd8ba26bffc6357ca71c3787803abb5c7b043
Stas Andriana <astas@pc76.infra.zone>
2025-05-12 13:35:53 +0300

    adding revconcatalterante

---
+++ 65%/revconcatalternate/revconcatalternate.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func RevConcatAlternate(slice1, slice2 []int) []int {
+	len1 := len(slice1)
+	len2 := len(slice2)
+	maxLen := len1
+	result := make([]int, 0, len1+len2)
+
+	if len2 > maxLen {
+		maxLen = len2
+	}
+
+	for i := maxLen; i >= 0; i-- {
+		if i < len1 {
+			result = append(result, slice1[i])
+		}
+		if i < len2 {
+			result = append(result, slice2[i])
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3, 9, 8}, []int{4, 5}))
+	fmt.Println(RevConcatAlternate([]int{1, 2, 3}, []int{}))
+}
