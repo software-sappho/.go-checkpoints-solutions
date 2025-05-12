46f2314a866cc6fba296cdbfd9d04882775226fa
Stas Andriana <astas@pc76.infra.zone>
2025-05-12 13:51:00 +0300

    fixing fprime

--- 50%/fprime/fprime.go
+++ 50%/fprime/fprime.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
+// Function to convert string to int manually
+func stringToInt(s string) (int, bool) {
+	result := 0
+	for _, c := range s {
+		if c < '0' || c > '9' {
+			return 0, false
+		}
+		result = result*10 + int(c-'0')
+	}
+	return result, true
+}
+
 func fprime(value int) {
-	if value == 1 || value == 0{
+	if value == 1 || value == 0 {
 		return
 	}
 	divisionIterator := 2
@@ -28,11 +39,10 @@ func fprime(value int) {
 
 func main() {
 	if len(os.Args) == 2 {
-		if i, err := strconv.Atoi(os.Args[1]); err == nil {
-			fprime(i)
+		numStr := os.Args[1]
+		num, valid := stringToInt(numStr)
+		if valid {
+			fprime(num)
 		}
 	}
 }
-
-
-// check if works at actual checkpoint
